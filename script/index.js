767d72cb4c84007b345e100e525944b5b6bceee5
Tianye Chen <78762926+tianye-chen@users.noreply.github.com>
2022-05-04 18:04:25 -0400

    display

--- script/index.js
+++ script/index.js
@@ -1,8 +1,21 @@
 const sqlite3 = require("sqlite3").verbose();
+
 const db = new sqlite3.Database("./lab2.db", sqlite3.OPEN_READWRITE, (err) => {
     if (err) return console.error(err.message);
-
     console.log("No errors");
+});
+
+const insert = `INSERT INTO Cars(Timestamp, Email, Name, Year, Make, Model, Car_ID, Judge_ids)
+                VALUES(?,?,?,?,?,?,?,?)`;
+
+const displayCars = `SELECT * FROM Cars`;
+
+db.all(displayCars, [], (err, rows) => {
+    if (err) return console.error(err.message);
+
+    rows.forEach((row) => {
+        console.log(row);
+    });
 });
 
 db.close((err) => {
