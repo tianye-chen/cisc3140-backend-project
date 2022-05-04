b84606f4b00763b105d53e97856062f1ee77b9b0
Tianye Chen <78762926+tianye-chen@users.noreply.github.com>
2022-05-04 17:50:20 -0400

    setup

--- script/index.js
+++ script/index.js
@@ -1,1 +1,10 @@
 const sqlite3 = require("sqlite3").verbose();
+const db = new sqlite3.Database("./lab2.db", sqlite3.OPEN_READWRITE, (err) => {
+    if (err) return console.error(err.message);
+
+    console.log("No errors");
+});
+
+db.close((err) => {
+    if (err) return console.error(err.message);
+});
