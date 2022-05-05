6c8bc03eaf7c6b204b374f3e4e5cdeb4806c9614
Tianye Chen <78762926+tianye-chen@users.noreply.github.com>
2022-05-04 22:39:05 -0400

    json response

--- script/index.js
+++ script/index.js
@@ -5,7 +5,7 @@ const sqlite3 = require("sqlite3").verbose();
 
 const db = new sqlite3.Database("./lab2.db", sqlite3.OPEN_READWRITE, (err) => {
     if (err) return console.error(err.message);
-    console.log("No errors");
+    console.log("Successfully opened database");
 });
 
 const insertCars = `INSERT INTO Cars(Timestamp, Email, Name, Year, Make, Model, Car_ID, Judge_ids)
@@ -18,26 +18,41 @@ const displayCars = `SELECT * FROM Cars`;
 db.all(displayCars, [], (err, rows) => {
     if (err) return console.error(err.message);
 
-    rows.forEach((row) => {
-        console.log(row);
+    console.log(rows[0]);
+});
+
+app.listen(PORT, () => {
+    console.log(`PORT ${PORT}`);
+});
+
+app.get("/", (req, res) => {
+    db.serialize(() => {
+        db.each(
+            "SELECT name NAME FROM Cars WHERE Car_ID = ?",
+            [1],
+            (err, rows) => {
+                if (err) {
+                    res.send("Error while displaying");
+                }
+                res.json(rows);
+            }
+        );
     });
 });
 
-app.listen(PORT,()=>{
-        console.log(`PORT ${PORT}`)
-    })
-
-app.get('/', (req, res) => {
-    db.serialize(()=>{
-        db.each(displayCars,(err,row)=>{
-            if(err){
-                res.send("Error while displaying")
+app.get("/all", (req, res) => {
+    db.serialize(() => {
+        db.all(displayCars,[],(err, rows) => {
+                if (err) {
+                    res.send("Error while displaying");
+                }
+                res.json(rows);
             }
-            res.send(`Timestamp: ${row.Timestamp}`)
-        })
-    })
+        );
+    });
 });
 
+/*
 db.close((err) => {
     if (err) return console.error(err.message);
-});
+});*/
