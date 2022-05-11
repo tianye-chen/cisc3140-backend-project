d9f4235fb3cd76176682cde5017ade8c205ee35f
Tianye Chen <78762926+tianye-chen@users.noreply.github.com>
2022-05-11 12:26:25 -0400

    fix stuff

--- script/index.js
+++ script/index.js
@@ -12,7 +12,7 @@ const db = new sqlite3.Database("./lab2.db", sqlite3.OPEN_READWRITE, (err) => {
 const insertCars = `INSERT INTO Cars(Timestamp, Email, Name, Year, Make, Model, Car_ID, Judge_ids)
                 VALUES(?,?,?,?,?,?,?,?)`;
 
-const updateCars = `UPDATE Cars SET Timestamp = ?, Email = ?, Name = ?, Year = ?, Make = ?, Model = ?, Judge_ids = ? WHERE Car_ID = ? VALUE(?,?,?,?,?,?,?)`;
+const updateCars = `UPDATE Cars SET Timestamp = ?, Email = ?, Name = ?, Year = ?, Make = ?, Model = ?, Judge_ids = ? WHERE Car_ID = ? VALUE(?,?,?,?,?,?,?,?)`;
 
 const updateJudges = `UPDATE Judges SET Car_ids = ?, JUDGES_Name = ? WHERE Judge_ID = ? VALUE(?,?)`;
 
@@ -96,7 +96,7 @@ app.get("/car/update/:id/:updateCar", (req, res) => {
         updateCar.Make,
         updateCar.Model,
         updateCar.Judge_ids,
-        id,
+        parseInt(id),
     ]);
     res.send(updateCar);
 });
@@ -109,11 +109,11 @@ app.get(
             req.params.Timestamp,
             req.params.Email,
             req.params.Name,
-            req.params.Year,
+            parseInt(req.params.Year),
             req.params.Make,
             req.params.Model,
             req.params.Judge_ids,
-            req.params.Car_ID,
+            parseInt(req.params.Car_ID),
         ]);
         res.send(req.params.Car_ID);
     }
@@ -166,14 +166,21 @@ app.get("/judge/add/:newJudge", (req, res) => {
 });
 
 // Add judge by params
-app.get("/judge/add/:Car_ids/:Judge_ID/:Judge_Name", (req, res) => {
-    db.run(insertJudge, [
-        req.params.Car_ids,
-        req.params.Judge_ID,
-        req.params.Judge_Name,
-    ]);
-    res.send(req.params.Judge_ID);
-});
+app.get(
+    "/judge/add/:Car_ids/:Judge_ID/:Judge_Name/:CarsJudged/:starttime/:endtime/:minspent/:avgspd",
+    (req, res) => {
+        db.run(insertJudge, [
+            req.params.Car_ids,
+            req.params.Judge_ID,
+            req.params.Judge_Name,
+            req.params.CarsJudged,
+            req.params.starttime,
+            req.params.minspent,
+            req.params.avgspd,
+        ]);
+        res.send(req.params.Judge_ID);
+    }
+);
 
 // Add judge by query, param must be array
 app.get("/judge/add/query/param", (req, res) => {
